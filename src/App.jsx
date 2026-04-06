ca3ed4eb0e54f24031fe2ba1e04d9f517557aeb0
mdsabbirvuia <mdsabbir3225@gmail.com>
2026-04-06 18:25:46 +0600

    add navbar and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './components/navbar/navbar'
+
+function App() {
+
+  return (
+    <>
+    <nav>
+      <Navbar></Navbar>
+    </nav>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2>Logo</h2>
+            <ul>
+                <li>home</li>
+                <li>info</li>
+                <li>blog</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
